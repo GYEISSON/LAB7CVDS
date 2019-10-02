85f6006645323c51f282b506fa7bfb3d3e9ec8f6
GYEISSON <yeison7f@gmail.com>
2019-10-02 18:59:56 -0500

    import

--- src/main/java/edu/eci/cvds/samples/services/client/MyBatisExample.java
+++ src/main/java/edu/eci/cvds/samples/services/client/MyBatisExample.java
@@ -15,8 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package edu.eci.cvds.samples.services.client;
-package edu.eci.cvds.sampleprj.dao.mybatis.mappers.*;
-package
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.*;
+import edu.eci.cvds.samples.entities.TipoItem.*;
 
 
 import java.io.IOException;
